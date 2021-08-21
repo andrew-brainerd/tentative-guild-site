f29219e537fa5960bab56e8c94566b140492af36
Andrew Brainerd <drwb333@gmail.com>
2021-08-21 03:12:37 -0400

    Add Warcraft Logs button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import guildLogo from './guild-logo.png';
 import discordLogo from './discord-logo.svg';
+import warcraftLogsLogo from './warcraft-logs-logo.png';
 import './App.css';
 
 function App() {
@@ -10,9 +11,15 @@ function App() {
         <h1>
           &lt;Tentative&gt;
         </h1>
-        <a href="https://discord.gg/ZGnM9BxRNw" target="_blank" rel="noreferrer">
-          <img src={discordLogo} className="discord-logo" alt="Discord Logo" />
-        </a>
+        <section className="guild-buttons">
+          <a href="https://discord.gg/ZGnM9BxRNw" target="_blank" rel="noreferrer">
+            <img src={discordLogo} className="discord-logo" alt="Discord Logo" />
+          </a>
+          <a href="https://classic.warcraftlogs.com/guild/id/610120" className="warcraft-logs-button" target="_blank" rel="noreferrer">
+            <img src={warcraftLogsLogo} className="warcraft-logs-logo" alt="Warcraft Logs Logo" />
+            <span className="button-text">Warcraft Logs</span>
+          </a>
+        </section>
       </header>
     </div>
   );
