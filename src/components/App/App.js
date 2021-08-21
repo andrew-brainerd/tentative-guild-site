996b8998567764014f2b02a088b219d516f59da8
Andrew Brainerd <drwb333@gmail.com>
2021-08-21 17:42:33 -0400

    Move components into directory

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,30 @@
-import guildLogo from './guild-logo.png';
-import discordLogo from './discord-logo.svg';
-import warcraftLogsLogo from './warcraft-logs-logo.png';
+import Header from '../Header/Header';
+import guildLogo from '../../img/guild-logo.png';
+import discordLogo from '../../img/discord-logo.svg';
+import warcraftLogsLogo from '../../img/warcraft-logs-logo.png';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="app">
-      <header>
+      <Header />
+      <section className="content">
         <img src={guildLogo} className="guild-logo" alt="Guild Logo" />
         <h1>
           &lt;Tentative&gt;
         </h1>
         <section className="guild-buttons">
-          <a href="https://discord.gg/ZGnM9BxRNw" target="_blank" rel="noreferrer">
+          <a href="https://discord.gg/ZGnM9BxRNw" className="button" target="_blank" rel="noreferrer">
             <img src={discordLogo} className="discord-logo" alt="Discord Logo" />
           </a>
-          <a href="https://classic.warcraftlogs.com/guild/id/610120" className="warcraft-logs-button" target="_blank" rel="noreferrer">
+          <a href="https://classic.warcraftlogs.com/guild/id/610120" className="button warcraft-logs-button" target="_blank" rel="noreferrer">
             <img src={warcraftLogsLogo} className="warcraft-logs-logo" alt="Warcraft Logs Logo" />
             <span className="button-text">Warcraft Logs</span>
           </a>
         </section>
-      </header>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
