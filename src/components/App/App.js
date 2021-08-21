c0a368102a27a2d67d57842207f203518ec6b7ab
Andrew Brainerd <drwb333@gmail.com>
2021-08-21 17:50:28 -0400

    Prevent button animation on mobile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { isMobile } from 'react-device-detect';
 import Header from '../Header/Header';
 import guildLogo from '../../img/guild-logo.png';
 import discordLogo from '../../img/discord-logo.svg';
@@ -14,10 +15,27 @@ const App = () => {
           &lt;Tentative&gt;
         </h1>
         <section className="guild-buttons">
-          <a href="https://discord.gg/ZGnM9BxRNw" className="button" target="_blank" rel="noreferrer">
+          <a
+            href="https://discord.gg/ZGnM9BxRNw"
+            className={[
+              "button",
+              !isMobile ? "animate" : ""
+            ].join(' ')}
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={discordLogo} className="discord-logo" alt="Discord Logo" />
           </a>
-          <a href="https://classic.warcraftlogs.com/guild/id/610120" className="button warcraft-logs-button" target="_blank" rel="noreferrer">
+          <a
+            href="https://classic.warcraftlogs.com/guild/id/610120"
+            className={[
+              "button",
+              "warcraft-logs-button",
+              !isMobile ? "animate" : ""
+            ].join(' ')}
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={warcraftLogsLogo} className="warcraft-logs-logo" alt="Warcraft Logs Logo" />
             <span className="button-text">Warcraft Logs</span>
           </a>
