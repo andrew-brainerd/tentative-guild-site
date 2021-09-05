d4f59739b826be504327edcfe7920e43379d89d9
Andrew Brainerd <drwb333@gmail.com>
2021-09-05 12:58:40 -0400

    Update about information

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,17 +6,14 @@ const About = () => {
     <div className={styles.content}>
       <h1>About Us</h1>
       <div className={styles.about}>
-        <p>
-          Welcome to Tentative! Our main focus is on TBC Classic.
-          We are consisted of experience players who are always looking to progress themselves.
-          Always in discord hanging out, always running normal and heroic dungeons and always leveling.
-          We encourage everyone to work within the guild and to help each other out.
-          There are gaps between officers, raiders, members and casuals.
-          Everyone plays the game with each other and no one is left out.
-          We expect everyone to be somewhat active and online more often than not.
-          As mentioned above, please work within the guild as much as possible.
-          Running a normal for reputation? Ask the guild first. Selling some primals? Ask the guild please!
-        </p>
+        <h2>Welcome to Tentative! </h2>
+        <ul>
+          <li>Our current focus is Phase 2 of TBC Classic.</li>
+          <li>We are made up of experience players who are always looking to progress.</li>
+          <li>We love to hang out in Discord and always have members running normal and heroic dungeons.</li>
+          <li>We encourage everyone to work within the guild and to help each other out.</li>
+          <li>Everyone loves to play the game with each other and no one is left out.</li>
+        </ul>
       </div>
     </div>
   );
