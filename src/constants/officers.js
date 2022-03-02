9f4f5fd9cfb3a31a798886cb3c3c6330f2fad402
Andrew Brainerd <drwb333@gmail.com>
2022-03-02 15:55:51 -0500

    Remove chumps

--- src/constants/officers.js
+++ src/constants/officers.js
@@ -1,7 +1,6 @@
 import techPortrait from '../img/techanalyst.png';
 import raechPortrait from '../img/raech.png';
-import stanPortrait from '../img/standarsho.png';
-import comingSoonPortrait from '../img/coming-soon.svg';
+// import comingSoonPortrait from '../img/coming-soon.svg';
 
 export default [
   {
@@ -12,38 +11,10 @@ export default [
     title: 'Guild Master'
   },
   {
-    name: 'Abortio',
-    race: 'Gnome',
-    class: 'Warlock',
-    img: comingSoonPortrait,
-    title: 'Officer'
-  },
-  {
-    name: 'Amandus',
-    race: 'Human',
-    class: 'Priest',
-    img: comingSoonPortrait,
-    title: 'Officer'
-  },
-  {
-    name: 'Mortablockin',
-    race: 'Human',
-    class: 'Paladin',
-    img: comingSoonPortrait,
-    title: 'Officer'
-  },
-  {
     name: 'Raech',
     race: 'Draenei',
     class: 'Hunter',
     img: raechPortrait,
-    title: 'Officer'
-  },
-  {
-    name: 'Standarsho',
-    race: 'Human',
-    class: 'Paladin',
-    img: stanPortrait,
-    title: 'Officer'
+    title: 'Senior Officer'
   }
 ];
